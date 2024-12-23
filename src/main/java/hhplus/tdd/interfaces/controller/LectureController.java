94b664bb3e9efb28cddd95f6f7e40e8e5a0f3f8c
CHOHYUNHWA <gusghk115@gmail.com>
2024-12-24 01:10:57 +0900

    init: presentation Layer 선언 - 컨트롤러 설정 - facade 생성자 주입 선언

--- src/main/java/hhplus/tdd/interfaces/controller/LectureController.java
+++ src/main/java/hhplus/tdd/interfaces/controller/LectureController.java
@@ -1,4 +1,15 @@
 package hhplus.tdd.interfaces.controller;
 
+import hhplus.tdd.application.facade.LectureFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+@RequiredArgsConstructor
 public class LectureController {
+
+    private final LectureFacade lectureFacade;
+
 }
