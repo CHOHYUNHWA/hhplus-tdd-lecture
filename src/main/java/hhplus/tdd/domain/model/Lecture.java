da0822e9a0ade28476e8eb20f864e071c57e5dbd
CHOHYUNHWA <gusghk115@gmail.com>
2024-12-24 14:50:30 +0900

    feat: Lecture Model 객체 테스트 및 기능 구현

--- src/main/java/hhplus/tdd/domain/model/Lecture.java
+++ src/main/java/hhplus/tdd/domain/model/Lecture.java
@@ -1,6 +1,8 @@
 package hhplus.tdd.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import hhplus.tdd.global.exception.CustomException;
+import hhplus.tdd.global.exception.ErrorCode;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -18,4 +20,26 @@ public record Lecture (
     Integer currentCapacity
 ){
 
+    public void checkLectureDate() {
+        if(LocalDateTime.now().isAfter(this.lectureDt)) {
+            throw new CustomException(ErrorCode.LECTURE_ALREADY_ENDED);
+        }
+    }
+
+    public void checkCapacity() {
+        if(this.currentCapacity >= this.capacity) {
+            throw new CustomException(ErrorCode.EXCEEDED_REGISTRATION_LIMIT);
+        }
+    }
+
+    public Lecture increaseCapacity() {
+        return Lecture.builder()
+                .id(this.id)
+                .title(this.title)
+                .lecturer(this.lecturer)
+                .capacity(this.capacity)
+                .currentCapacity(this.currentCapacity + 1)
+                .lectureDt(this.lectureDt)
+                .build();
+    }
 }
--- src/main/java/hhplus/tdd/global/exception/ErrorCode.java
+++ src/main/java/hhplus/tdd/global/exception/ErrorCode.java
@@ -10,7 +10,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다."),
-    ALREADY_REGISTERED(HttpStatus.CONFLICT, "이미 특강을 신청하셨습니다.");
+    ALREADY_REGISTERED(HttpStatus.CONFLICT, "이미 특강을 신청하셨습니다."),
+    LECTURE_ALREADY_ENDED(HttpStatus.BAD_REQUEST, "해당 강의는 이미 종료되었습니다."),
+    EXCEEDED_REGISTRATION_LIMIT(HttpStatus.BAD_REQUEST, "잔여 신청 인원 수가 부족합니다."),
+    NOT_EXIST_LECTURE(HttpStatus.NOT_FOUND, "존재하지 않는 강의 입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
