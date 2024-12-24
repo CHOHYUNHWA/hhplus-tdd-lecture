4d88b55211e2ddcd8c37ebfd44f75899e395055f
CHOHYUNHWA <gusghk115@gmail.com>
2024-12-24 09:11:23 +0900

    init : LectureController 기본 구조 구현

---
+++ src/main/java/hhplus/tdd/api/web/LectureController.java
@@ -0,0 +1,64 @@
+package hhplus.tdd.api.web;
+
+import hhplus.tdd.application.dto.AvailableLectureResponse;
+import hhplus.tdd.application.dto.CompletedRegistrationLecturesResponse;
+import hhplus.tdd.application.dto.LectureRegisterResponse;
+import hhplus.tdd.application.dto.RegistrationRequest;
+import hhplus.tdd.application.facade.LectureFacade;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/lectures")
+@RequiredArgsConstructor
+public class LectureController {
+
+    private final LectureFacade lectureFacade;
+
+    /**
+     * 1️⃣ 특강 신청 API
+     * 특정 userId로 선착순으로 제공되는 특강을 신청합니다.
+     * - 동일 사용자는 동일 강의에 대해 한 번만 신청 가능
+     * - 정원이 30명을 초과하면 실패
+     */
+    @PostMapping("/register")
+    public ResponseEntity<LectureRegisterResponse> applyForLecture(
+            @RequestBody RegistrationRequest registrationRequest
+            ) {
+        LectureRegisterResponse lectureRegisterResponse = lectureFacade.registerLecture(registrationRequest);
+        return new ResponseEntity<>(lectureRegisterResponse, HttpStatus.OK);
+    }
+
+    /**
+     * 2️⃣ 특강 신청 가능 목록 API
+     * 날짜별로 현재 신청 가능한 특강 목록을 조회합니다.
+     * - 정원은 30명으로 고정
+     * - 사용자가 신청 가능한 특강 목록 조회
+     */
+    @GetMapping
+    public ResponseEntity<AvailableLectureResponse> getAvailableLectures(
+            @RequestParam LocalDateTime date
+    ) {
+        AvailableLectureResponse availableLectureResponse = lectureFacade.getAvailableLectures(date);
+        return new ResponseEntity<>(availableLectureResponse, HttpStatus.OK);
+    }
+
+    /**
+     * 3️⃣ 특정 유저 특강 신청 완료 목록 조회 API
+     * 특정 userId로 신청 완료된 특강 목록을 조회합니다.
+     * - 각 항목은 특강 ID, 이름, 강연자 정보를 포함
+     */
+    @GetMapping("/{studentId}")
+    public ResponseEntity<Void> getCompletedRegistrationLectures(
+            @PathVariable("studentId") long studentId
+    ) {
+        CompletedRegistrationLecturesResponse completedRegistrationLecturesResponse = lectureFacade.getCompletedRegistrationLectures(studentId);
+        return null;
+    }
+}
--- src/main/java/hhplus/tdd/application/facade/LectureFacade.java
+++ src/main/java/hhplus/tdd/application/facade/LectureFacade.java
@@ -1,10 +1,16 @@
 package hhplus.tdd.application.facade;
 
+import hhplus.tdd.application.dto.AvailableLectureResponse;
+import hhplus.tdd.application.dto.CompletedRegistrationLecturesResponse;
+import hhplus.tdd.application.dto.LectureRegisterResponse;
+import hhplus.tdd.application.dto.RegistrationRequest;
 import hhplus.tdd.domain.repository.LectureRepository;
 import hhplus.tdd.domain.service.LectureService;
 import hhplus.tdd.domain.service.RegistrationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -13,4 +19,18 @@ public class LectureFacade {
     private final LectureService lectureService;
     private final RegistrationService registrationService;
 
+    public LectureRegisterResponse registerLecture(RegistrationRequest registrationRequest) {
+
+        return null;
+    }
+
+    public AvailableLectureResponse getAvailableLectures(LocalDateTime date) {
+
+        return null;
+    }
+
+    public CompletedRegistrationLecturesResponse getCompletedRegistrationLectures(long studentId) {
+
+        return null;
+    }
 }
